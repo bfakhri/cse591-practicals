ecc8a717277e32fb43fc8297ece5aa9f3df1de04
Bijan Fakhri <bfakhri@asu.edu>
2017-02-17 23:06:33 -0700

    f

---
+++ miniprojects/miniproject3/final.py
@@ -0,0 +1,53 @@
+#import os
+#os.environ['THEANO_FLAGS'] = "force_device=True, device=gpu"
+#import theano
+#print("Theano Default Device: ")
+#print(theano.config.device)
+
+from yann.network import network
+net = network()
+dataset_params  = { "dataset": "_mini3/_mini3", "id": 'mnist', "n_classes" : 10 }
+net.add_layer(type = "input", id ="input", dataset_init_args = dataset_params)
+
+net.add_layer (type = "dot_product",
+               origin ="input",
+               id = "dot_product_1",
+               num_neurons = 800,
+               regularize = True,
+               activation ='relu')
+
+net.add_layer (type = "dot_product",
+               origin ="dot_product_1",
+               id = "dot_product_2",
+               num_neurons = 800,
+               regularize = True,
+               activation ='relu')
+
+net.add_layer ( type = "classifier",
+                id = "softmax",
+                origin = "dot_product_2",
+                num_classes = 10,
+                activation = 'softmax',
+                )
+
+net.add_layer ( type = "objective",
+                id = "nll",
+                origin = "softmax",
+                )
+
+optimizer_params =  {
+            #"momentum_type"       : 'nesterov',
+            "momentum_type"       : 'polyak',
+            #"momentum_params"     : (0.9, 0.95, 25),
+            "momentum_params"     : (0.9, 0.95, 30),
+            #"regularization"      : (0.1, 0.2),
+            "regularization"      : (0.01, 0.02),
+            "optimizer_type"      : 'rmsprop',
+            #"optimizer_type"      : 'adagrad',
+            "id"                  : 'bij'
+                    }
+net.add_module ( type = 'optimizer', params = optimizer_params )
+
+
+learning_rates = (0.05, 0.01, 0.001)
+

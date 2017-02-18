83753ec77b6b995a30dac7b777c7088cb3977f4d
Bijan Fakhri <bfakhri@asu.edu>
2017-02-17 23:07:38 -0700

    f

--- miniprojects/miniproject3/final.py
+++ miniprojects/miniproject3/final.py
@@ -51,3 +51,18 @@ net.add_module ( type = 'optimizer', params = optimizer_params )
 
 learning_rates = (0.05, 0.01, 0.001)
 
+net.cook( optimizer = 'bij',
+          objective_layer = 'nll',
+          datastream = 'mnist',
+          classifier = 'softmax',
+          )
+
+net.train( epochs = (20, 20),
+           validate_after_epochs = 2,
+           training_accuracy = True,
+           learning_rates = learning_rates,
+           show_progress = True,
+           early_terminate = True)
+
+net.test()
+~                

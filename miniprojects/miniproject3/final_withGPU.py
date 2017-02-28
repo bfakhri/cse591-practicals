a48d8dd265a5edef7589703ceca341a738a5d2da
Bijan Fakhri (Student) <bfakhri@en4113734l.fulton.ad.asu.edu>
2017-02-28 16:57:56 -0700

    t

--- miniprojects/miniproject3/final_withGPU.py
+++ miniprojects/miniproject3/final_withGPU.py
@@ -1,5 +1,4 @@
 import os
-#os.environ['THEANO_FLAGS'] = "force_device=True, device=gpu, lib.cnmem=0.75, floatX=float32, mode=DebugMode,DebugMode.check_py=False"
 os.environ['THEANO_FLAGS'] = "force_device=True, device=gpu, lib.cnmem=0.75, floatX=float32"
 import theano
 print("Theano Default Device: ")
@@ -7,38 +6,11 @@ print(theano.config.device)
 import sys	# For CLI args
 from yann.network import network
 net = network()
-
-dataset_params  = { "dataset": "_datasets/_dataset_72566", "id": 'mnist', "n_classes" : 10 }
-
-# Begin ConvNet Setup
+dataset_params  = { "dataset": sys.argv[1], "id": 'mnist', "n_classes" : 10 }
 net.add_layer(type = "input", id ="input", dataset_init_args = dataset_params)
 
-net.add_layer ( type = "conv_pool",
-                origin = "input",
-                id = "conv_pool_1",
-                num_neurons = 40,
-                filter_size = (5,5),
-                pool_size = (2,2),
-                activation = ('maxout', 'maxout', 2),
-                batch_norm = True,
-                regularize = True,
-                verbose = True
-            )
-
-net.add_layer ( type = "conv_pool",
-                origin = "conv_pool_1",
-                id = "conv_pool_2",
-                num_neurons = 20,
-                filter_size = (3,3),
-                pool_size = (2,2),
-                activation = ('maxout', 'maxout', 2),
-                batch_norm = True,
-                regularize = True,
-                verbose = True 
-            )
-
 net.add_layer (type = "dot_product",
-               origin ="conv_pool_2",
+               origin ="input",
                id = "dot_product_1",
                num_neurons = 800,
                regularize = True,
@@ -47,7 +19,7 @@ net.add_layer (type = "dot_product",
 net.add_layer (type = "dot_product",
                origin ="dot_product_1",
                id = "dot_product_2",
-               num_neurons = 100,
+               num_neurons = 800,
                regularize = True,
                activation ='relu')
 
@@ -69,7 +41,7 @@ optimizer_params =  {
             #"momentum_params"     : (0.9, 0.95, 25),
             "momentum_params"     : (0.9, 0.95, 30),
             #"regularization"      : (0.1, 0.2),
-            "regularization"      : (0.001, 0.002),
+            "regularization"      : (0.01, 0.02),
             "optimizer_type"      : 'rmsprop',
             #"optimizer_type"      : 'adagrad',
             "id"                  : 'bij'
@@ -77,8 +49,7 @@ optimizer_params =  {
 net.add_module ( type = 'optimizer', params = optimizer_params )
 
 
-#learning_rates = (0.05, 0.01, 0.001)
-learning_rates = (0.05, 0.005, 0.001)
+learning_rates = (0.05, 0.01, 0.001)
 
 net.cook( optimizer = 'bij',
           objective_layer = 'nll',

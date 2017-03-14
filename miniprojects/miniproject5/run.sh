dc1db6dc5eee15c0c73d06c4f90668c94a62734a
Bijan Fakhri (Student) <bfakhri@en4113734l.fulton.ad.asu.edu>
2017-03-14 13:09:38 -0700

    t

--- miniprojects/miniproject5/run.sh
+++ miniprojects/miniproject5/run.sh
@@ -3,3 +3,9 @@ python Di_r.py _datasets/_dataset_21382/
 python Di_r.py _datasets/_dataset_41981/
 python Di_r.py _datasets/_dataset_57444/
 
+python Dj_Di.py _datasets/_dataset_21382/ _datasets/_dataset_41981/
+python Dj_Di.py _datasets/_dataset_21382/ _datasets/_dataset_57444/
+python Dj_Di.py _datasets/_dataset_41981/ _datasets/_dataset_21382/
+python Dj_Di.py _datasets/_dataset_41981/ _datasets/_dataset_57444/
+python Dj_Di.py _datasets/_dataset_57444/ _datasets/_dataset_41981/
+python Dj_Di.py _datasets/_dataset_57444/ _datasets/_dataset_21382/
